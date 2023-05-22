0d11e440054b7e3ea22da06d368d9064cf0fc498
Armando <armandomm98@hotmail.com>
2023-05-21 23:26:47 -0600

    reviews with html, css and tsx completed

---
+++ reviews/src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { reviews } from "./data";
+import { FaQuoteRight, FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const { id, image, name, job, text } = reviews[index];
+  const nextReview = () => {
+    setIndex((prev) => {
+      return (prev + 1) % reviews.length;
+    });
+  };
+
+  const prevReview = () => {
+    setIndex((prev) => {
+      return (prev - 1 + reviews.length) % reviews.length;
+    });
+  };
+
+  const randomReview = () => {
+    setIndex((prev) => {
+      let randomNumber = Math.floor(Math.random() * reviews.length);
+      if (randomNumber === index) {
+        randomNumber = prev + 1;
+      }
+      return randomNumber % reviews.length;
+    });
+  };
+
+  return (
+    <main className="main">
+      <section className="section">
+        <div className="section-title">
+          <h2>reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <article className="review">
+          <div className="img-container">
+            <img src={image} alt={name} className="image-review" />
+            <span className="icon-quote">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+          <div className="btn-container">
+            <button className="prev-btn" type="button" onClick={prevReview}>
+              <FaAngleLeft />
+            </button>
+            <button className="next-btn" type="button" onClick={nextReview}>
+              <FaAngleRight />
+            </button>
+          </div>
+          <button
+            type="button"
+            className="btn btn-review"
+            onClick={randomReview}
+          >
+            surprise me
+          </button>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ reviews/src/data.ts
@@ -0,0 +1,58 @@
+export interface Review {
+  id: number;
+  name: string;
+  image: string;
+  job: string;
+  text: string;
+}
+
+export const reviews: Review[] = [
+  {
+    id: 1,
+    name: "carol dw",
+    image:
+      "https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    job: "front-end developer",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error incidunt, vel voluptatibus expedita repudiandae nobis sint commodi dolore modi nemo?",
+  },
+  {
+    id: 2,
+    name: "harry mt",
+    image:
+      "https://images.pexels.com/photos/5397723/pexels-photo-5397723.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    job: "back-end developer",
+    text: "     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus sint doloremque quidem vero tempore ipsum perferendis voluptatibus impedit possimus in!",
+  },
+  {
+    id: 3,
+    name: "kamiz fa",
+    image:
+      "https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    job: "ux designer",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum repudiandae recusandae blanditiis voluptates vitae dignissimos delectus quam quibusdam facere veritatis.",
+  },
+  {
+    id: 4,
+    name: "spencer sv",
+    image:
+      "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    job: "product owner",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab itaque dicta non tenetur distinctio aliquam provident autem quis, veniam blanditiis?",
+  },
+  {
+    id: 5,
+    name: "lola rn",
+    image:
+      "https://images.pexels.com/photos/1855579/pexels-photo-1855579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    job: "web designer",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem, beatae recusandae iure eaque impedit officia dolores in quasi. Tempore.",
+  },
+  {
+    id: 6,
+    name: "masha rs",
+    image:
+      "https://images.pexels.com/photos/2853198/pexels-photo-2853198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    job: "cloud architect",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptates nesciunt ab rerum? Ad error sit eligendi aperiam asperiores ipsa!",
+  },
+];
